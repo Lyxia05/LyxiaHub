faaf60b188c22e33b734b96135f0a755fa735d59
Lyxia1 <161618940+Lyxia1@users.noreply.github.com>
2024-02-28 22:34:10 +0700

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -19,7 +19,7 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 --
-local RayfieldLibrary = loadstring(game:HttpGet('https://raw.githubusercontent.com/Lyxia1/LyxiaHub/main/UILib.lua'))()
+local RayfieldLibrary = loadstring(game:HttpGet('https://raw.githubusercontent.com/Lyxia1/LyxiaHub/main/UILIb.lua'))()
 
 
 
