721c0d9a59750667a4960d1ff85e320f28c7aacd
Lyxia1 <161618940+Lyxia1@users.noreply.github.com>
2024-02-29 15:18:12 +0700

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -66,7 +66,7 @@ end
 local function GetMobs()
 	for _, value in getgenv().Mobs do
 		local Mobs = MobsFolder:FindFirstChild(value)
-		if Mobs then
+		if Mobs and Mobs.HealthBar.Frame.HPBar.Fill.Bar.X.Scale > 0 then
 			getgenv().MobObject = Mobs
 			break
 		end
